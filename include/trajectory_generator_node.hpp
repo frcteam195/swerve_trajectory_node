a598633132caa6b68406325d05289ba98c76c4c8
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-25 22:26:27 -0400

    Generating trajectories.

--- include/trajectory_generator_node.hpp
+++ include/trajectory_generator_node.hpp
@@ -2,5 +2,9 @@
 
 #include <string>
 
+double max_acceleration;
+double max_velocity;
+double max_voltage;
+
 std::string trajectory_directory;
 
--- src/trajectory_generator_node.cpp
+++ src/trajectory_generator_node.cpp
@@ -1,6 +1,10 @@
 #include "trajectory_generator_node.hpp"
 
 #include "ck_utilities/ParameterHelper.hpp"
+#include "geometry/Pose2dWithCurvature.hpp"
+#include "plannners/DriveMotionPlanner.hpp"
+#include "trajectory/timing/TimingConstraint.hpp"
+#include "trajectory/Trajectory.hpp"
 #include "utils/JsonParser.hpp"
 
 #include "ros/ros.h"
@@ -18,20 +22,32 @@
 
 namespace fs = boost::filesystem;
 
-ros::NodeHandle* node;
+ros::NodeHandle *node;
 
 void generate_trajectories(void)
 {
     ROS_INFO("Generating all trajectories defined in: %s", trajectory_directory.c_str());
 
+    ck::planners::DriveMotionPlanner motion_planner;
+
     for (const fs::directory_entry &trajectory_configuration : fs::directory_iterator(trajectory_directory))
     {
         ROS_INFO("Generating trajectory from: %s", trajectory_configuration.path().c_str());
 
-        fs::ifstream trajectory_buffer { trajectory_configuration.path() };
+        fs::ifstream trajectory_buffer{trajectory_configuration.path()};
         nlohmann::json trajectory_json = nlohmann::json::parse(trajectory_buffer);
 
-        ck::json::parse_json_waypoints(trajectory_json);
+        std::vector<ck::geometry::Pose2d> waypoints = ck::json::parse_json_waypoints(trajectory_json["waypoints"]);
+
+        std::vector<ck::trajectory::timing::TimingConstraint<ck::geometry::Pose2dWithCurvature>> constraints;
+
+        ck::trajectory::Trajectory<ck::trajectory::timing::TimedState<ck::geometry::Pose2dWithCurvature>> output_trajectory;
+        output_trajectory = motion_planner.generateTrajectory(trajectory_json["reversed"],
+                                                              waypoints,
+                                                              constraints,
+                                                              max_acceleration,
+                                                              max_velocity,
+                                                              max_voltage);
     }
 }
 
@@ -39,7 +55,7 @@ bool get_trajectory(trajectory_generator_node::GetTrajectory::Request &request, 
 {
     ROS_INFO("Getting trajectory: %s", request.path_name.c_str());
 
-	response.success = false;
+    response.success = false;
     return false;
 }
 
@@ -62,15 +78,18 @@ int main(int argc, char **argv)
     node = &n;
 
     bool required_params_found = true;
-	required_params_found &= n.getParam(CKSP(trajectory_directory), trajectory_directory);
+    required_params_found &= n.getParam(CKSP(max_acceleration), max_acceleration);
+    required_params_found &= n.getParam(CKSP(max_velocity), max_velocity);
+    required_params_found &= n.getParam(CKSP(max_voltage), max_voltage);
+    required_params_found &= n.getParam(CKSP(trajectory_directory), trajectory_directory);
 
-	if (!required_params_found)
-	{
-		ROS_ERROR("Missing required parameters for node %s. Please check the list and make sure all required parameters are included", ros::this_node::getName().c_str());
-		return 1;
-	}
-    
-	ros::ServiceServer service_generate = node->advertiseService("get_trajectory", get_trajectory);
+    if (!required_params_found)
+    {
+        ROS_ERROR("Missing required parameters for node %s. Please check the list and make sure all required parameters are included", ros::this_node::getName().c_str());
+        return 1;
+    }
+
+    ros::ServiceServer service_generate = node->advertiseService("get_trajectory", get_trajectory);
 
     ros::spin();
     return 0;
