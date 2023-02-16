df47a6a4a4a3ab8bda574f35235319f2d6e4e812
BonomiC <bonomi.c11@gmail.com>
2023-02-16 00:51:04 +0000

    Update

--- src/parsers/JsonParser.cpp
+++ src/parsers/JsonParser.cpp
@@ -9,15 +9,15 @@ namespace ck
 {
     namespace json
     {
-        std::pair<std::vector<team254_geometry::Pose2d>, std::vector<team254_geometry::Rotation2d>> parse_json_waypoints(nlohmann::json json_waypoints)
+        pair<vector<Pose2d>, vector<Rotation2d>> parse_json_waypoints(nlohmann::json json_waypoints)
         {
-            std::pair<std::vector<team254_geometry::Pose2d>, std::vector<team254_geometry::Rotation2d>> output;
+            pair<vector<Pose2d>, vector<Rotation2d>> output;
 
             for (auto json_waypoint : json_waypoints)
             {
-                team254_geometry::Rotation2d track = team254_geometry::Rotation2d::fromDegrees(json_waypoint["track"]);
-                team254_geometry::Pose2d waypoint(json_waypoint["x"], json_waypoint["y"], track);
-                team254_geometry::Rotation2d heading = team254_geometry::Rotation2d::fromDegrees(json_waypoint["heading"]);
+                Rotation2d track = Rotation2d::fromDegrees(json_waypoint["track"]);
+                Pose2d waypoint(json_waypoint["x"], json_waypoint["y"], track);
+                Rotation2d heading = Rotation2d::fromDegrees(json_waypoint["heading"]);
                 output.first.push_back(waypoint);
                 output.second.push_back(heading);
             }
